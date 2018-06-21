762a42dbbe530b55e99b67ddfb354cbe27c682b2
Sam G. Zun <zunsamuel97@gmail.com>
2018-06-20 20:42:19 -0500

    Updating all Files

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,12 +6,36 @@ const apiRoutes = (app) => {
     })
 
     app.post('/api/friends', (req,res) => {
-        const newFriend = {
+        const match = {
             name: '',
             image: '',
-            values: []
+            isMatch: '',
         }
+
+        const user = req.body;
+        const userVals = user.values;
+
+        for (let i = 0; i < Friends.length; i++) {
+            const friendData = Friends[i];
+            let dif;
+    
+            for (let j = 0; j < friendData.values.length; j++) {
+                const friendDataVals = friendData.values[j];
+                const currentUserVals = userVals[j];
+        
+                dif += Math.abs(+currentUserVals - +friendDataVals);
+            }
+    
+            if (dif <= match.isMatch) {
+                match.name = friendData.name;
+                match.image = friendData.photo;
+                match.isMatch = dif;
+            }
+        }
+
+        Friends.push(user);
+        res.json(match)
     })
 }
 
-export default apiRoutes
+export default apiRoutes;
